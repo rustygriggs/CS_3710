e1207510d7ac5f8c074843fb1ba12ed4b3cbbbc9
Patrick Armstrong <Patrick Armstrong>
2016-10-19 19:05:10 -0600

    Rusty forgets stuff

---
+++ ComputerV1.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    18:35:24 10/19/2016 
+// Design Name: 
+// Module Name:    ComputerV1 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module ComputerV1(
+    );
+
+
+endmodule
