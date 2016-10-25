515f17c0b9552135eeab6a8a2071ea74a2da38fb
Preston Baker <pbaker000@gmail.com>
2016-10-25 16:19:24 -0600

    added core

---
+++ ComputerV1/Core.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    14:48:59 10/25/2016 
+// Design Name: 
+// Module Name:    Core 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Core(
+	input clk,
+	input [15:0] inData,
+	output [15:0] outData
+    );
+	 
+
+endmodule
